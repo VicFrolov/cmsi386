5fe1a9c8abf5bd2cabae5d95db47d9376c0c075a
Victor Frolov <vicfrolov@gmail.com>
2015-11-15 23:59:57 -0800

    implemented compile, compileToString and some extra evals

--- hw5/hw5.java
+++ hw5/hw5.java
@@ -1,39 +1,43 @@
-/* Name:
+/* Name: Victor Frolov
 
-   UID:
+   UID: 978687700
 
-   Others With Whom I Discussed Things:
+   Others With Whom I Discussed Things: Peyton
 
-   Other Resources I Consulted:
+   Other Resources I Consulted: StackExchange for some errors I was getting
    
 */
 
-// import lists and other data structures from the Java standard library
-// DO NOT IMPORT ANYTHING ELSE
 import java.util.*;
 
-
-     
-// a type for arithmetic expressions
 interface AExp {
-    Double eval();                         // Problem 1
-    // List<AInstr> compile();                 // Problem 3
+    Double eval(); 
+    List<AInstr> compile();             
+    List<String> compileToString();
 }
-
-
 
 class Num implements AExp {
     protected Double val;
 
     public Num(Double n) {
-      this.val = n;
+        this.val = n;
     }
 
     public Double eval() {
-     return this.val;
+        return this.val;
     }
 
+    public List<AInstr> compile() {
+        List<AInstr> num = new LinkedList<AInstr>();
+        num.add(new Push (val));
+        return num;
+    }
 
+    public List<String> compileToString() {
+        List<String> num = new LinkedList<String>();
+        num.add("Push "+ val.toString());
+        return num;
+    }
 }
 
 class BinOp implements AExp {
@@ -49,9 +53,38 @@ class BinOp implements AExp {
     public Double eval() {
         return op.calculate(left.eval(), right.eval());
     }
+
+    public List<AInstr> compile() {
+        List<AInstr> list = new LinkedList<AInstr>();
+        
+        if (this.left != null) {
+            list.addAll(left.compile());
+        }
+        if (this.right != null) {
+            list.addAll(right.compile());
+        }
+        
+        list.add(new Calculate(op));
+        
+        return list;
+    }
+
+    
+    public List<String> compileToString() {
+        List<String> test = new LinkedList<String>();
+        
+        if (this.left != null) {
+            test.addAll(this.left.compileToString());
+        }
+        if (this.right != null) {
+            test.addAll(this.right.compileToString());
+        }
+        
+        test.add("Calculate " + op.toString());
+        return  test;
+    }    
 }
 
-// a representation of four arithmetic operators
 enum Op {
     PLUS { public Double calculate(Double a1, Double a2) { return a1 + a2; } },
     MINUS { public Double calculate(Double a1, Double a2) { return a1 - a2; } },
@@ -61,31 +94,60 @@ enum Op {
     abstract Double calculate(Double a1, Double a2);
 }
 
-// a type for arithmetic instructions
 interface AInstr {
-    // void eval(Stack<Double> stack);    // Problem 2
+    void eval(Stack<Double> stack);  
 }
 
 class Push implements AInstr {
     protected Double val;
+
+    public Push(Double n) {
+        this.val = n;
+    }
+
+    public void eval(Stack<Double> stack) {
+        stack.push(val);
+    }
+
 }
 
 class Calculate implements AInstr {
     protected Op op;
+    
+    public Calculate(Op o) {
+        this.op = o;
+    }
+
+    public void eval(Stack<Double> stack) {
+        Double d1 = stack.pop();
+        Double d2 = stack.pop();
+        Double result = op.calculate(d1, d2);
+        stack.push(result);
+    }
 }
 
 class Instrs {
     protected List<AInstr> instrs;
 
-    public Instrs(List<AInstr> instrs) { this.instrs = instrs; }
+    public Instrs(List<AInstr> instrs) { 
+        this.instrs = instrs; 
+    }
 
-    // public Double eval() {}  // Problem 2
+    public Double eval() {
+        Stack<Double> stackEvaluation = new Stack<Double>();
+        
+        for(AInstr instrs : instrs) {
+            instrs.eval(stackEvaluation);
+        }
+
+        return stackEvaluation.pop();
+    }
 }
 
 
 class CalcTest {
     public static void main(String[] args) {
-      // a test for Problem 1
+        // Tests for Problem 1
         AExp aexp =
             new BinOp(new BinOp(new Num(1.0), Op.PLUS, new Num(2.0)),
             Op.TIMES,
@@ -104,18 +166,32 @@ class CalcTest {
             new Num(4.0));
         System.out.println("aexp evaluates to " + aexp3.eval() + " and expected 64.0"); // aexp evaluates to 64.0        
 
-  // a test for Problem 2
-  // List<AInstr> is = new LinkedList<AInstr>();
-  // is.add(new Push(1.0));
-  // is.add(new Push(2.0));
-  // is.add(new Calculate(Op.PLUS));
-  // is.add(new Push(4.0));
-  // is.add(new Calculate(Op.TIMES));
-  // Instrs instrs = new Instrs(is);
-  // System.out.println("instrs evaluates to " + instrs.eval());  // instrs evaluates to 12.0
+        // Tests for Problem 2
+        List<AInstr> is = new LinkedList<AInstr>();
+        is.add(new Push(1.0));
+        is.add(new Push(2.0));
+        is.add(new Calculate(Op.PLUS));
+        is.add(new Push(4.0));
+        is.add(new Calculate(Op.TIMES));
+        Instrs instrs = new Instrs(is);
+        System.out.println("instrs evaluates to " + instrs.eval());  // instrs evaluates to 12.0
 
-  // a test for Problem 3
-  // System.out.println("aexp converts to " + aexp.compile());
+        List<AInstr> is2 = new LinkedList<AInstr>();
+        is2.add(new Push(100.0));
+        is2.add(new Push(40.0));
+        is2.add(new Calculate(Op.MINUS));
+        is2.add(new Push(4.0));
+        is2.add(new Calculate(Op.TIMES));
+        Instrs instrs2 = new Instrs(is2);
+        System.out.println("instrs evaluates to " + instrs2.eval() + " and expected -240.0");
+
+        // Tests for Problem 3
+        System.out.println("aexp converts to " + aexp.compile());
+        System.out.println("aexp converts to " + aexp.compileToString());
+        System.out.println("aexp2 converts to " + aexp2.compile());
+        System.out.println("aexp2 converts to " + aexp2.compileToString());
+        System.out.println("aexp3 converts to " + aexp3.compile());
+        System.out.println("aexp3 converts to " + aexp3.compileToString());
 
     }
 }
